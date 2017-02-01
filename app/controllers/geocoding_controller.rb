4ceaba8cd54764378882360d37ffc9bcdbd4570c
Alex Markowitz <admarko@gmail.com>
2017-01-31 23:47:52 -0600

    finished street to cords

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -9,17 +9,13 @@ class GeocodingController < ApplicationController
   def street_to_coords
     @street_address = params[:user_street_address]
 
-    # ==========================================================================
-    # Your code goes below.
-    #
-    # The street address that the user typed is in the variable @street_address.
-    # ==========================================================================
+    @street_address_without_spaces = @street_address.gsub(' ','+')
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" << @street_address_without_spaces << "&sensor=false"
+    @parsed_data = JSON.parse(open(url).read)
 
 
-
-    @latitude = "Replace this string with your answer."
-
-    @longitude = "Replace this string with your answer."
+    @latitude = @parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = @parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
